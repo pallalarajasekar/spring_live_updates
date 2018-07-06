b887625d08d03b6e8214158ab739e7a69b670f17
pallalarajasekar <pallalalrajasekar@192.168.0.4>
2018-07-06 01:26:08 -0700

    code commit

---
+++ src/main/java/com/websock/WebsockApplication.java
@@ -0,0 +1,12 @@
+package com.websock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsockApplication.class, args);
+	}
+}
